86da5f1f9c7eed2caad6ce643d8e83f735196857
Karyna Kananovich <karyna.kananovich@gmail.com>
2021-08-23 12:47:47 +0300

    feat: add shop state

--- src/pages/Shop.vue
+++ src/pages/Shop.vue
@@ -2,9 +2,22 @@
   <div class="wrapper-content wrapper-content--fixed">
     <section>
       <div class="container">
-        <h1>
-          Shop page</h1>
+        <h1>Shop page</h1>
       </div>
     </section>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      shopList: null,
+    };
+  },
+  created() {
+    this.shopList = this.$store.getters.getShopList;
+    console.log(this.shopList);
+  },
+};
+</script>
---
+++ src/store/shop.js
@@ -0,0 +1,20 @@
+export default {
+  state: {
+    shopList: [
+      {
+        id: 1,
+        title: 'Nike Red',
+        descr:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        img: require('../assets/img/1.png'),
+      },
+    ],
+  },
+  mutations: {},
+  actions: {},
+  getters: {
+    getShopList(state) {
+      return state.shopList;
+    },
+  },
+};
